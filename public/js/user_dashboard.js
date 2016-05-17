ccc72e2584dcd305622e4e45b2b3ec9c2694c160
Adarsh <rajshahbvm@yahoo.com>
2016-05-17 11:25:22 +0530

    lead interaction view added + code segregation (js with html)

--- public/js/user_dashboard.js
+++ public/js/user_dashboard.js
@@ -1,78 +1,13 @@
 /**
  * Created by batcoder1 on 16/5/16.
  */
-var localityArray = [];
-var candidateLocalityArray = [];
-var jobArray = [];
-var candidateJobArray = [];
-function getLocality(){
-    return localityArray;
-}
-function getJob(){
-    return jobArray;
-}
 
-function getCandidateLocality(){
-    return candidateLocalityArray;
-}
-
-function getCandidateJob(){
-    return candidateJobArray;
-}
-
-function processDataCheck(returnedData) {
-    var parent = $('.preferredLocation')[0];
-    returnedData.forEach(function (locality) {
-        var l = document.createElement("li");
-        l.textContent = locality.locality.localityName;
-        parent.appendChild(l);
-
-        var id = locality.localityId;
-        var name = locality.locality.localityName;
-        var item = {}
-        item ["id"] = id;
-        item ["name"] = name;
-        candidateLocalityArray.push(item);
-    });
-}
-function processDataCheckJob(returnedData) {
-    var parent = $('.preferredJobs')[0];
-    returnedData.forEach(function (job) {
-        var l = document.createElement("li");
-        l.textContent = job.jobRole.jobName;
-        parent.appendChild(l);
-
-        var id = job.jobRole.jobRoleId;
-        var name = job.jobRole.jobName;
-        var item = {}
-        item ["id"] = id;
-        item ["name"] = name;
-        candidateJobArray.push(item);
-    });
-}
-function processDataCheckLocality(returnedData) {
-    returnedData.forEach(function(locality)
-    {
-        var id = locality.localityId;
-        var name = locality.localityName;
-        var item = {}
-        item ["id"] = id;
-        item ["name"] = name;
-        localityArray.push(item);
-    });
-}
-function processDataCheckJobs(returnedData) {
-    returnedData.forEach(function(job)
-    {
-        var id = job.jobId;
-        var name = job.jobName;
-        var item = {}
-        item ["id"] = id;
-        item ["name"] = name;
-        jobArray.push(item);
-    });
-}
 $(document).ready(function(){
+    var localityArray = [];
+    var candidateLocalityArray = []; //candidate preferred localities
+    var jobArray = [];
+    var candidateJobArray = []; //candidate preferred Job roles
+
     try {
         $.ajax({
             type: "GET",
@@ -80,7 +15,7 @@ $(document).ready(function(){
             data: false,
             contentType: false,
             processData: false,
-            success: processDataCheckLocality
+            success: processDataCheckAllLocalies
         });
     } catch (exception) {
         console.log("exception occured!!" + exception);
@@ -92,7 +27,7 @@ $(document).ready(function(){
             data: false,
             contentType: false,
             processData: false,
-            success: processDataCheckJobs
+            success: processDataCheckAllJobRoles
         });
     } catch (exception) {
         console.log("exception occured!!" + exception);
@@ -104,7 +39,7 @@ $(document).ready(function(){
             data: false,
             contentType: false,
             processData: false,
-            success: processDataCheck
+            success: processDataGetCandidateLocality
         });
     } catch (exception) {
         console.log("exception occured!!" + exception);
@@ -116,11 +51,12 @@ $(document).ready(function(){
             data: false,
             contentType: false,
             processData: false,
-            success: processDataCheckJob
+            success: processDataGetCandidateJobRoles
         });
     } catch (exception) {
         console.log("exception occured!!" + exception);
     }
+
 
     var userMobile = localStorage.getItem("mobile");
     var userName = localStorage.getItem("name");
@@ -138,48 +74,87 @@ $(document).ready(function(){
         window.location = "/";
     }
 
-});
-function logoutUser() {
-    localStorage.clear();
-    try {
+    function processDataGetCandidateLocality(returnedData) {
+        var parent = $('.preferredLocation')[0];
+        returnedData.forEach(function (locality) {
+            var l = document.createElement("li");
+            l.textContent = locality.locality.localityName;
+            parent.appendChild(l);
 
-        $.ajax({
-            type: "GET",
-            url: "/logoutUser",
-            data: false,
-            contentType: false,
-            processData: false,
+            var id = locality.localityId;
+            var name = locality.locality.localityName;
+            var item = {}
+            item ["id"] = id;
+            item ["name"] = name;
+            candidateLocalityArray.push(item);
         });
-    } catch (exception) {
-        console.log("exception occured!!" + exception);
     }
-    window.location = "/";
-}
 
-var options = {'showRowNumber': true};
-var data;
-var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1HwEWPzZD4BFCyeRf5HO_KqNXyaMporxYQfg5lhOoA2g/edit#gid=496359801');
+    function processDataGetCandidateJobRoles(returnedData) {
+        var parent = $('.preferredJobs')[0];
+        returnedData.forEach(function (job) {
+            var l = document.createElement("li");
+            l.textContent = job.jobRole.jobName;
+            parent.appendChild(l);
 
-function sendAndDraw() {
-    var val = localStorage.getItem("mobile");
-    query.setQuery('select C where C=' + val.substring(3, 13));
-    query.send(handleQueryResponse);
-}
+            var id = job.jobRole.jobRoleId;
+            var name = job.jobRole.jobName;
+            var item = {}
+            item ["id"] = id;
+            item ["name"] = name;
+            candidateJobArray.push(item);
+        });
+    }
 
-function handleQueryResponse(response) {
-    if (response.isError()) {
-        return;
+    function processDataCheckAllLocalies(returnedData) {
+        returnedData.forEach(function(locality)
+        {
+            var id = locality.localityId;
+            var name = locality.localityName;
+            var item = {}
+            item ["id"] = id;
+            item ["name"] = name;
+            localityArray.push(item);
+        });
     }
-    data = response.getDataTable();
-    new google.visualization.Table(document.getElementById('table')).draw(data, options);
-    var data2 = document.getElementsByClassName('google-visualization-table-td google-visualization-table-td-number').length;
-    if(data2>0) {
-        $('#doneAssessment').show();
-        $('#takeAssessment').hide();
+    function processDataCheckAllJobRoles(returnedData) {
+        returnedData.forEach(function(job)
+        {
+            var id = job.jobId;
+            var name = job.jobName;
+            var item = {}
+            item ["id"] = id;
+            item ["name"] = name;
+            jobArray.push(item);
+        });
     }
-    else{
-        $('#doneAssessment').hide();
-        $('#takeAssessment').show();
+    
+    var options = {'showRowNumber': true};
+    var data;
+    var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1HwEWPzZD4BFCyeRf5HO_KqNXyaMporxYQfg5lhOoA2g/edit#gid=496359801');
+
+    function sendAndDraw() {
+        var val = localStorage.getItem("mobile");
+        query.setQuery('select C where C=' + val.substring(3, 13));
+        query.send(handleQueryResponse);
     }
-}
-google.setOnLoadCallback(sendAndDraw);
+
+    function handleQueryResponse(response) {
+        if (response.isError()) {
+            return;
+        }
+        data = response.getDataTable();
+        new google.visualization.Table(document.getElementById('table')).draw(data, options);
+        var data2 = document.getElementsByClassName('google-visualization-table-td google-visualization-table-td-number').length;
+        if(data2>0) {
+            $('#doneAssessment').show();
+            $('#takeAssessment').hide();
+        }
+        else{
+            $('#doneAssessment').hide();
+            $('#takeAssessment').show();
+        }
+    }
+    google.setOnLoadCallback(sendAndDraw);
+});
+
--- public/support/js/interaction_controller.js
+++ public/support/js/interaction_controller.js
@@ -4,15 +4,19 @@
 
 function processDataForSupportInteraction(returnedData) {
 
-    var t = $('table#interactionHistory').DataTable();
+    var t = $('table#interactionHistory').DataTable({
+        "order": [[ 0, "desc" ]]
+    });
     //DoTheDue Here
     returnedData.forEach(function (interaction) {
         if(interaction.user_id != null){
             t.row.add( [
+                interaction.user_interaction_timestamp,
                 interaction.user_id,
                 interaction.user_name,
-                interaction.user_interaction_timestamp,
-                interaction.user_note
+                interaction.user_interactionType,
+                interaction.user_note,
+                interaction.user_results,
             ] ).draw( false );
         } else {
             console.log("Null obj encountered");
@@ -21,12 +25,19 @@ function processDataForSupportInteraction(returnedData) {
     NProgress.done();
 }
 
+function showInteraction(x) {
+    window.location = "/candidateInteraction/" + x.id;
+}
+
 $(function(){
+    var pathname = window.location.pathname; // Returns path only
+    var leadIdUrl = pathname.split('/');
+    var leadId = leadIdUrl[(leadIdUrl.length)-1];
     try {
         NProgress.start();
         $.ajax({
             type: "GET",
-            url: "/getCandidateInteraction/" + 1,
+            url: "/getCandidateInteraction/" + leadId,
             data: false,
             contentType: false,
             processData: false,
