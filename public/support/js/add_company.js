c7b2dbc845819822347ec40999da411e3dcd3f21
Adarsh <rajshahbvm@yahoo.com>
2016-06-26 19:49:32 +0530

    company validation added

--- public/support/js/add_company.js
+++ public/support/js/add_company.js
@@ -11,28 +11,42 @@ function processDataAddCompany(returnedData) {
 $(function() {
     $("#company_form").submit(function(eventObj) {
         eventObj.preventDefault();
-        try {
-            var d = {
-                companyName: $("#companyName").val(),
-                companyEmployeeCount: $("#companyEmployeeCount").val(),
-                companyWebsite: $("#companyWebsite").val(),
-                companyDescription: $("#companyDescription").val(),
-                companyAddress: $("#companyAddress").val(),
-                companyPinCode: $("#companyPinCode").val(),
-                companyLogo: $("#companyLogo").val(),
-                companyLocality: parseInt($("#companyLocality").val()),
-                companyType: $("#companyType").val(),
-                companyStatus: $("#companyStatus").val()
-            };
-            $.ajax({
-                type: "POST",
-                url: "/addCompany",
-                contentType: "application/json; charset=utf-8",
-                data: JSON.stringify(d),
-                success: processDataAddCompany
-            });
-        } catch (exception) {
-            console.log("exception occured!!" + exception);
+        var status = 1;
+        if($("#companyName").val() == ""){
+            alert("Please Enter company Name");
+            status=0;
+        } else if($("#companyLocality").val() == ""){
+            alert("Please Enter company Locality");
+            status=0;
+        } else if($("#companyLogo").val() == ""){
+            alert("Please Enter company Logo");
+            status=0;
         }
+        if(status == 1){
+            try {
+                var d = {
+                    companyName: $("#companyName").val(),
+                    companyEmployeeCount: $("#companyEmployeeCount").val(),
+                    companyWebsite: $("#companyWebsite").val(),
+                    companyDescription: $("#companyDescription").val(),
+                    companyAddress: $("#companyAddress").val(),
+                    companyPinCode: $("#companyPinCode").val(),
+                    companyLogo: $("#companyLogo").val(),
+                    companyLocality: parseInt($("#companyLocality").val()),
+                    companyType: $("#companyType").val(),
+                    companyStatus: $("#companyStatus").val()
+                };
+                $.ajax({
+                    type: "POST",
+                    url: "/addCompany",
+                    contentType: "application/json; charset=utf-8",
+                    data: JSON.stringify(d),
+                    success: processDataAddCompany
+                });
+            } catch (exception) {
+                console.log("exception occured!!" + exception);
+            }
+        }
+
     }); // end of submit
 }); // end of function
