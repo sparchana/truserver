e8a77c26a71a4b6a7d8ae7b9848bc0ba646cbe7e
Adarsh <rajshahbvm@yahoo.com>
2016-07-07 18:48:44 +0530

    default 0 added

---
+++ conf/evolutions/default/6.sql
@@ -0,0 +1,21 @@
+# --- !Ups
+
+alter table jobpost add column jobrecruiterid bigint signed not null default 0;
+alter table recruiterprofile add column reccompany bigint signed not null;
+
+alter table jobpost add constraint fk_jobpost_jobrecruiterid foreign key (jobrecruiterid) references recruiterprofile (recruiterprofileid) on delete restrict on update restrict;
+create index ix_jobpost_jobrecruiterid on jobpost (jobrecruiterid);
+
+alter table recruiterprofile add constraint fk_recruiterprofile_reccompany foreign key (reccompany) references company (companyid) on delete restrict on update restrict;
+create index ix_recruiterprofile_reccompany on recruiterprofile (reccompany);
+
+# --- !Downs
+
+alter table recruiterprofile drop column reccompany;
+alter table jobpost drop column jobrecruiterid;
+
+alter table recruiterprofile drop foreign key fk_recruiterprofile_reccompany;
+drop index ix_recruiterprofile_reccompany on recruiterprofile;
+
+alter table jobpost drop foreign key fk_jobpost_jobrecruiterid;
+drop index ix_jobpost_jobrecruiterid on jobpost;
