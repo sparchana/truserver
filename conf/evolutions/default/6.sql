15824af131f16db55b24faf44e9510b5ef551d18
Adarsh <rajshahbvm@yahoo.com>
2016-07-13 20:48:18 +0530

    6.sql modified

--- conf/evolutions/default/6.sql
+++ conf/evolutions/default/6.sql
@@ -11,11 +11,12 @@ create index ix_recruiterprofile_reccompany on recruiterprofile (reccompany);
 
 # --- !Downs
 
-alter table recruiterprofile drop column reccompany;
-alter table jobpost drop column jobrecruiterid;
-
 alter table recruiterprofile drop foreign key fk_recruiterprofile_reccompany;
 drop index ix_recruiterprofile_reccompany on recruiterprofile;
 
 alter table jobpost drop foreign key fk_jobpost_jobrecruiterid;
 drop index ix_jobpost_jobrecruiterid on jobpost;
+
+alter table recruiterprofile drop column reccompany;
+alter table jobpost drop column jobrecruiterid;
+
