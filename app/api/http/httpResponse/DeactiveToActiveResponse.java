a47945080281e91bc81daf8be14daac490c908d9
zero <ethereal.sandeep@gmail.com>
2016-07-19 19:11:33 +0530

    admin tab for deactivatedCandidateList

---
+++ app/api/http/httpResponse/DeactiveToActiveResponse.java
@@ -0,0 +1,28 @@
+package api.http.httpResponse;
+
+import models.entity.Candidate;
+
+import java.util.List;
+
+/**
+ * Created by zero on 19/7/16.
+ */
+public class DeactiveToActiveResponse {
+    public static final int STATUS_SUCCESS = 1;
+    public static final int STATUS_FAILURE = 2;
+
+    public int status;
+    public List<Candidate> candidateList;
+
+    public List<Candidate> getCandidateList() {
+        return candidateList;
+    }
+
+    public void setCandidateList(List<Candidate> candidateList) {
+        this.candidateList = candidateList;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
