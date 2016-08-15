8772517027ed2b29672636286f1065704ec5ab41
sparchana <archana@stylekall.com>
2016-08-15 11:37:56 +0530

    evolution script for candidate latlong usage

---
+++ conf/evolutions/default/10.sql
@@ -0,0 +1,19 @@
+# --- !Ups
+alter table candidate add column candidateplacelat double null;
+alter table candidate add column candidateplacelng double null;
+
+alter table jobposttolocality add column latitude double null;
+alter table jobposttolocality add column longitude double null;
+
+alter table jobrole add column jobroleicon varchar(255) null;
+
+# --- !Downs
+
+alter table candidate drop column candidateplacelat;
+alter table candidate drop column candidateplacelng;
+
+alter table jobposttolocality drop column latitude;
+alter table jobposttolocality drop column longitude;
+
+alter table jobrole drop column jobroleicon;
+
