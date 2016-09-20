e23aa6b7a6f71b78a7eb25c190dd152cb6a3e985
ethereal-sandeep <ethereal.sandeep@gmail.com>
2016-09-20 15:25:51 +0530

    sql preshift code

--- conf/evolutions/default/14.sql
+++ conf/evolutions/default/14.sql
@@ -1,55 +1,5 @@
 # --- !Ups
 
-create table assessment_question (
-  assessment_question_id        int unsigned auto_increment not null,
-  question_text                 text null,
-  assessment_question_type_id   int unsigned null,
-  optiona                       text null,
-  optionb                       text null,
-  optionc                       text null,
-  optiond                       text null,
-  optione                       text null,
-  answer                        text null,
-  jobroleid                     bigint signed null,
-  skillid                       int signed null,
-  constraint pk_assessment_question primary key (assessment_question_id)
-);
-
-alter table assessment_question add constraint fk_assessment_question_jobroleid foreign key (jobroleid) references jobrole (jobroleid) on delete restrict on update restrict;
-create index ix_assessment_question_jobroleid on assessment_question (jobroleid);
-
-alter table assessment_question add constraint fk_assessment_question_skillid foreign key (skillid) references skill (skillid) on delete restrict on update restrict;
-create index ix_assessment_question_skillid on assessment_question (skillid);
-
-create table assessment_question_type (
-  assessment_question_type_id   int unsigned auto_increment not null,
-  assessment_question_type_title varchar(255) null,
-  constraint pk_assessment_question_type primary key (assessment_question_type_id)
-);
-
-alter table assessment_question add constraint fk_assessment_question_assessment_question_type_id foreign key (assessment_question_type_id) references assessment_question_type (assessment_question_type_id) on delete restrict on update restrict;
-create index ix_assessment_question_assessment_question_type_id on assessment_question (assessment_question_type_id);
-
-
-create table candidate_assessment_response (
-  ca_response_id                bigint signed auto_increment not null,
-  candidateid                   bigint signed,
-  jobpostid                     bigint signed,
-  jobroleid                     bigint signed,
-  result                        text null,
-  constraint pk_candidate_assessment_response primary key (ca_response_id)
-);
-
-alter table candidate_assessment_response add constraint fk_candidate_assessment_response_candidateid foreign key (candidateid) references candidate (candidateid) on delete restrict on update restrict;
-create index ix_candidate_assessment_response_candidateid on candidate_assessment_response (candidateid);
-
-alter table candidate_assessment_response add constraint fk_candidate_assessment_response_jobpostid foreign key (jobpostid) references jobpost (jobpostid) on delete restrict on update restrict;
-create index ix_candidate_assessment_response_jobpostid on candidate_assessment_response (jobpostid);
-
-alter table candidate_assessment_response add constraint fk_candidate_assessment_response_jobroleid foreign key (jobroleid) references jobrole (jobroleid) on delete restrict on update restrict;
-create index ix_candidate_assessment_response_jobroleid on candidate_assessment_response (jobroleid);
-
-
 create table partner_to_candidate (
   partner_to_candidate_id       bigint signed auto_increment not null,
   partner_to_candidate_create_timestamp timestamp not null default current_timestamp,
@@ -65,6 +15,7 @@ create index ix_partner_to_candidate_partner_id on partner_to_candidate (partner
 
 alter table partner_to_candidate add constraint fk_partner_to_candidate_candidate_candidateid foreign key (candidate_candidateid) references candidate (candidateid) on delete restrict on update restrict;
 
+alter table auth add column otp int not null default 0;
 # --- !Downs
 
 alter table partner_to_candidate drop foreign key fk_partner_to_candidate_partner_id;
@@ -72,30 +23,6 @@ drop index ix_partner_to_candidate_partner_id on partner_to_candidate;
 
 alter table partner_to_candidate drop foreign key fk_partner_to_candidate_candidate_candidateid;
 
-alter table assessment_question drop foreign key fk_assessment_question_jobroleid;
-drop index ix_assessment_question_jobroleid on assessment_question;
-
-alter table assessment_question drop foreign key fk_assessment_question_skillid;
-drop index ix_assessment_question_skillid on assessment_question;
-
-alter table assessment_question drop foreign key fk_assessment_question_assessment_question_type_id;
-drop index ix_assessment_question_assessment_question_type_id on assessment_question;
-
-alter table candidate_assessment_response drop foreign key fk_candidate_assessment_response_candidateid;
-drop index ix_candidate_assessment_response_candidateid on candidate_assessment_response;
-
-alter table candidate_assessment_response drop foreign key fk_candidate_assessment_response_jobpostid;
-drop index ix_candidate_assessment_response_jobpostid on candidate_assessment_response;
-
-alter table candidate_assessment_response drop foreign key fk_candidate_assessment_response_jobroleid;
-drop index ix_candidate_assessment_response_jobroleid on candidate_assessment_response;
-
-
 drop table if exists partner_to_candidate;
 
-drop table if exists assessment_question;
-
-drop table if exists assessment_question_type;
-
-drop table if exists candidate_assessment_response;
-
+alter table auth drop column otp;
