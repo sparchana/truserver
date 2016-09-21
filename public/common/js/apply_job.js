7aeb769d676270240aa2ad55009bfe52e7c3e347
adarsh <rajshahbvm@yahoo.com>
2016-09-21 17:19:50 +0530

    UI tweaks

--- public/common/js/apply_job.js
+++ public/common/js/apply_job.js
@@ -12,8 +12,9 @@ var education;
 var homeLocality;
 
 function processDataApplyJob(returnedData) {
-    $("#messagePromptModal").modal("show");
     $("#jobApplyConfirm").modal("hide");
+    $("#messagePromptModal").modal("show");
+    $('body').addClass('open-modal');
     if(returnedData.status == 1){
         $('#customMsgIcon').attr('src', "/assets/common/img/jobApplied.png");
         $("#customMsg").html("Your Job Application is Successful");
