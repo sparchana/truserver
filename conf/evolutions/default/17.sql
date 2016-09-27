1645519d7da6d21f4fa3565bb1edbbf15ad9e31d
adarsh <rajshahbvm@yahoo.com>
2016-09-27 15:37:26 +0530

    changed .sql files

--- conf/evolutions/default/17.sql
+++ conf/evolutions/default/17.sql
@@ -1,13 +1,38 @@
 # --- !Ups
 
-alter table jobapplication add column partner_id bigint signed;
+create table candidate_assessment_response (
+  ca_response_id                bigint signed auto_increment not null,
+  create_timestamp              timestamp not null default current_timestamp,
+  update_timestamp              timestamp null,
+  ca_attempt_id                 bigint signed,
+  assessment_question_id        int unsigned,
+  candidate_answer              text null,
+  score                         int null,
+  constraint pk_candidate_assessment_response primary key (ca_response_id)
+);
 
-alter table jobapplication add constraint fk_jobapplication_partner_id foreign key (partner_id) references partner (partner_id) on delete restrict on update restrict;
-alter table jobapplication add index ix_jobapplication_partner_id (partner_id)
+alter table candidate_assessment_response add constraint fk_candidate_assessment_response_ca_attempt_id foreign key (ca_attempt_id) references candidate_assessment_attempt (ca_attempt_id) on delete restrict on update restrict;
+create index ix_candidate_assessment_response_ca_attempt_id on candidate_assessment_response (ca_attempt_id);
+
+alter table candidate_assessment_response add constraint fk_candidate_assessment_response_assessment_question_id foreign key (assessment_question_id) references assessment_question (assessment_question_id) on delete restrict on update restrict;
+create index ix_candidate_assessment_response_assessment_question_id on candidate_assessment_response (assessment_question_id);
+
+alter table candidate_assessment_attempt drop column result;
+alter table candidate_assessment_attempt add result float null;
+
+alter table candidate_assessment_attempt add create_timestamp timestamp not null default current_timestamp;
 
 # --- !Downs
 
-alter table jobapplication drop foreign key fk_jobapplication_partner_id;
-drop index ix_jobapplication_partner_id on jobapplication;
+alter table candidate_assessment_response drop foreign key fk_candidate_assessment_response_ca_attempt_id;
+drop index ix_candidate_assessment_response_ca_attempt_id on candidate_assessment_response;
 
-alter table jobapplication drop column partner_id;
+alter table candidate_assessment_response drop foreign key fk_candidate_assessment_response_assessment_question_id;
+drop index ix_candidate_assessment_response_assessment_question_id on candidate_assessment_response;
+
+drop table if exists candidate_assessment_response;
+
+alter table candidate_assessment_attempt drop column result;
+alter table candidate_assessment_attempt add result text null;
+
+alter table candidate_assessment_attempt  drop column  create_timestamp;
