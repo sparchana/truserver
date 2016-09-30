ffdde87fa98c07537f3c9b3ee3bab6289fe34c65
ethereal-sandeep <ethereal.sandeep@gmail.com>
2016-09-30 20:59:42 +0530

    Interaction for assessment added

--- conf/evolutions/default/19.sql
+++ conf/evolutions/default/19.sql
@@ -14,6 +14,10 @@ create index ix_related_jobrole_job_role_id on related_jobrole (job_role_id);
 alter table related_jobrole add constraint fk_related_jobrole_related_job_role_id foreign key (related_job_role_id) references jobrole (jobroleid) on delete restrict on update restrict;
 create index ix_related_jobrole_related_job_role_id on related_jobrole (related_job_role_id);
 
+alter table candidate_assessment_attempt add column ca_attempt_uuid varchar(255) not null;
+
+-- after creating ca_attempt_uuid column in candidate_assessment_attempt, run UPDATE trujobsdev.candidate_assessment_attempt set ca_attempt_uuid = uuid()
+
 # --- !Downs
 
 alter table related_jobrole drop foreign key fk_related_jobrole_job_role_id;
@@ -21,5 +25,7 @@ drop index ix_related_jobrole_job_role_id on related_jobrole;
 
 alter table related_jobrole drop foreign key fk_related_jobrole_related_job_role_id;
 drop index ix_related_jobrole_related_job_role_id on related_jobrole;
+
+alter table candidate_assessment_attempt drop column ca_attempt_uuid;
 
 drop table if exists related_jobrole;
