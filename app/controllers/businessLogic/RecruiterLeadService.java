11ebf55f36b7357dddf036e5a5b55f1d8b7f1913
adarsh <rajshahbvm@yahoo.com>
2016-10-05 17:08:59 +0530

    theme added for mobile view

--- app/controllers/businessLogic/RecruiterLeadService.java
+++ app/controllers/businessLogic/RecruiterLeadService.java
@@ -1,48 +1,37 @@
 package controllers.businessLogic;
 
-import api.InteractionConstants;
 import api.ServerConstants;
 import api.http.FormValidator;
 import api.http.httpRequest.Recruiter.RecruiterLeadRequest;
 import api.http.httpResponse.Recruiter.RecruiterLeadResponse;
-import models.entity.Candidate;
-import models.entity.Interaction;
-import models.entity.Lead;
-import models.entity.OM.JobPreference;
 import models.entity.OM.RecruiterLeadToJobRole;
 import models.entity.OM.RecruiterLeadToLocality;
 import models.entity.RecruiterLead;
 import models.entity.Static.JobRole;
-import models.entity.Static.LeadSource;
 import models.entity.Static.Locality;
-import models.util.SmsUtil;
 import play.Logger;
 
 import javax.persistence.NonUniqueResultException;
 import java.util.ArrayList;
 import java.util.List;
 
-import static play.mvc.Controller.session;
-
 /**
  * Created by dodo on 5/10/16.
  */
 public class RecruiterLeadService {
-    public static RecruiterLead createOrUpdateConvertedRecruiterLead(String leadName, String leadMobile){
+    static RecruiterLead createOrUpdateConvertedRecruiterLead(String leadName, String leadMobile){
         RecruiterLead existingLead = isLeadExists(leadMobile);
         if(existingLead == null){
             RecruiterLead lead = new RecruiterLead(
-                    leadName,
-                    leadMobile,
-                    1
+                leadName,
+                leadMobile,
+                1
             );
             lead.setRecruiterLeadStatus(ServerConstants.LEAD_STATUS_WON);
             RecruiterLeadService.createLead(lead);
             Logger.info("New Lead Created Successfully");
             return lead;
-        }
-        else {
-            //TODO: No leadUpdateTimeStamp available though lead is updatable
+        } else {
             existingLead.setRecruiterLeadStatus(ServerConstants.LEAD_STATUS_WON);
             if(existingLead.getRecruiterLeadName().trim().isEmpty()){
                 existingLead.setRecruiterLeadName(leadName);
@@ -52,7 +41,7 @@ public class RecruiterLeadService {
         return existingLead;
     }
 
-    public static RecruiterLead isLeadExists(String mobile){
+    private static RecruiterLead isLeadExists(String mobile){
         try {
             RecruiterLead existingLead = RecruiterLead.find.where().eq("recruiter_lead_mobile", FormValidator.convertToIndianMobileFormat(mobile)).findUnique();
             if(existingLead != null) {
@@ -62,7 +51,7 @@ public class RecruiterLeadService {
         return null;
     }
 
-    public static void createLead(RecruiterLead lead){
+    private static void createLead(RecruiterLead lead){
         RecruiterLead existingLead = isLeadExists(FormValidator.convertToIndianMobileFormat(lead.getRecruiterLeadMobile()));
         if(existingLead == null) {
             RecruiterLead.addLead(lead);
