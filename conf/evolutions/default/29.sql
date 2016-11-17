bb7161d8beb0f0eda339dc8459fb1e59b86178ea
adarsh <rajshahbvm@yahoo.com>
2016-11-17 11:17:32 +0530

    sql file corrected

--- conf/evolutions/default/29.sql
+++ conf/evolutions/default/29.sql
@@ -2,14 +2,26 @@
 
 alter table idproofreference add column idproofnumber varchar(255) null;
 
-alter table interview_details add column latitude double(10,6) null;
-alter table interview_details add column longitude double(10,6) null;
-alter table interview_details add column placeid double(10,6) null;
+create table interview_status (
+  interview_status_id           bigint signed auto_increment not null,
+  interview_status_name         varchar(100) not null,
+  constraint pk_interview_status primary key (interview_status_id)
+);
+
+
+alter table jobapplication add column interview_status_id bigint signed;
+alter table jobapplication add column interviewstatuscomments varchar(5000) null;
+
+alter table jobapplication add constraint fk_jobapplication_interview_status_id foreign key (interview_status_id) references interview_status (interview_status_id) on delete restrict on update restrict;
+create index ix_jobapplication_interview_status_id on jobapplication (interview_status_id);
 
 # --- !Downs
 
-alter table idproofreference drop column idproofnumber;
+alter table jobapplication drop foreign key fk_jobapplication_interview_status_id;
+drop index ix_jobapplication_interview_status_id on jobapplication;
 
-alter table interview_details drop column latitude;
-alter table interview_details drop column longitude;
-alter table interview_details drop column placeid;
+drop table if exists interview_status;
+
+alter table jobapplication drop column interview_status_id;
+alter table jobapplication drop column interviewstatuscomments;
+alter table idproofreference drop column idproofnumber;
---
+++ conf/evolutions/default/31.sql
@@ -0,0 +1,30 @@
+# --- !Ups
+
+create table interview_confirmed_status_update (
+  interview_confirmed_status_update_id bigint unsigned auto_increment not null,
+  interview_confirmed_status_update_uuid varchar(255) not null,
+  create_timestamp              timestamp default current_timestamp not null,
+  update_timestamp              timestamp null,
+  job_post_workflow_id          bigint unsigned,
+  interview_confirmed_status_update_note text null,
+  constraint pk_interview_confirmed_status_update primary key (interview_confirmed_status_update_id)
+);
+
+alter table interview_confirmed_status_update add constraint fk_interview_confirmed_status_update_job_post_workflow_id foreign key (job_post_workflow_id) references job_post_workflow (job_post_workflow_id) on delete restrict on update restrict;
+create index ix_interview_confirmed_status_update_job_post_workflow_id on interview_confirmed_status_update (job_post_workflow_id);
+
+alter table interview_details add column latitude double(10,6) null;
+alter table interview_details add column longitude double(10,6) null;
+alter table interview_details add column placeid double(10,6) null;
+
+
+# --- !Downs
+
+alter table interview_confirmed_status_update drop foreign key fk_interview_confirmed_status_update_job_post_workflow_id;
+drop index ix_interview_confirmed_status_update_job_post_workflow_id on interview_confirmed_status_update;
+
+alter table interview_details drop column latitude;
+alter table interview_details drop column longitude;
+alter table interview_details drop column placeid;
+
+drop table if exists interview_confirmed_status_update;
