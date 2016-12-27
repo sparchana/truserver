68ebe9f59e8858812361549af1bc9a86f8886dc4
adarsh <rajshahbvm@yahoo.com>
2016-12-27 20:04:17 +0530

    expiring all the interview credits which are expiring today

--- app/controllers/scheduler/task/EODDebitInterviewCreditTask.java
+++ app/controllers/scheduler/task/EODDebitInterviewCreditTask.java
@@ -123,6 +123,64 @@ public class EODDebitInterviewCreditTask extends TimerTask {
         }).start();
     }
 
+    private void expireRecruiterInterviewCredits(){
+        new Thread(() -> {
+            Calendar today = Calendar.getInstance();
+            today.set(Calendar.HOUR_OF_DAY, 0);
+
+            String dateToday = today.get(Calendar.YEAR) + "-" + (today.get(Calendar.MONTH)+1) + "-" + today.get(Calendar.DATE);
+            List<RecruiterCreditPack> packList = RecruiterCreditPack.find.where()
+                    .eq("expiryDate", dateToday)
+                    .eq("RecruiterCreditCategory", ServerConstants.RECRUITER_CATEGORY_INTERVIEW_UNLOCK)
+                    .findList();
+
+            Integer creditsExpiring;
+            for(RecruiterCreditPack pack : packList){
+                creditsExpiring = pack.getCreditsAvailable();
+                pack.setCreditsAvailable(0);
+                pack.setCreditsUsed(pack.getCreditsUsed() + creditsExpiring);
+                pack.setCreditIsExpired(true);
+                pack.update();
+
+                //making an entry in recruiter credit history table
+                RecruiterCreditHistory recruiterCreditHistory = new RecruiterCreditHistory();
+                recruiterCreditHistory.setRecruiterProfile(pack.getRecruiterProfile());
+
+                RecruiterCreditCategory recruiterCreditCategory = RecruiterCreditCategory.find.where()
+                        .eq("recruiter_credit_category_id", ServerConstants.RECRUITER_CATEGORY_INTERVIEW_UNLOCK)
+                        .findUnique();
+
+                if(recruiterCreditCategory != null){
+                    recruiterCreditHistory.setRecruiterCreditCategory(recruiterCreditCategory);
+                }
+
+                RecruiterCreditHistory recruiterCreditHistoryLatest = RecruiterCreditHistory.find.where()
+                        .eq("RecruiterProfileId", pack.getRecruiterProfile().getRecruiterProfileId())
+                        .eq("RecruiterCreditCategory", ServerConstants.RECRUITER_CATEGORY_INTERVIEW_UNLOCK)
+                        .setMaxRows(1)
+                        .orderBy("create_timestamp desc")
+                        .findUnique();
+
+                if(recruiterCreditHistoryLatest != null){
+                    if(recruiterCreditHistoryLatest.getRecruiterCreditsAvailable() > 0){
+                        recruiterCreditHistory.setRecruiterCreditsAvailable(recruiterCreditHistoryLatest.getRecruiterCreditsAvailable() - creditsExpiring);
+                        recruiterCreditHistory.setRecruiterCreditsUsed(recruiterCreditHistoryLatest.getRecruiterCreditsUsed() + creditsExpiring);
+                        recruiterCreditHistory.setUnits(creditsExpiring);
+
+                        if(session().get("sessionUsername") != null){
+                            recruiterCreditHistory.setRecruiterCreditsAddedBy("Support: " + session().get("sessionUsername"));
+                        } else{
+                            recruiterCreditHistory.setRecruiterCreditsAddedBy("Not specified");
+                        }
+
+                        //saving/updating all the rows
+                        recruiterCreditHistory.save();
+                    }
+                }
+            }
+        }).start();
+    }
+
     @Override
     public void run() {
         // fetch all today's interviews
@@ -157,5 +215,8 @@ public class EODDebitInterviewCreditTask extends TimerTask {
 
         //crediting credits if feedback provided
         startCreditDebitTask(recruiterProfileList, false);
+
+        //task to expire all the interview credits which are expiring today
+        expireRecruiterInterviewCredits();
     }
 }
