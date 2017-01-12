9c77c5beed8e967c0b22d92aff0ce7df3ce0181e
ethereal-sandeep <ethereal.sandeep@gmail.com>
2017-01-12 17:43:04 +0530

    [ssl] ssl redirect takes config from app config file

--- app/controllers/security/ForceHttps.java
+++ app/controllers/security/ForceHttps.java
@@ -1,6 +1,5 @@
 package controllers.security;
 
-import play.api.Play;
 import play.mvc.Action;
 import play.mvc.Controller;
 import play.mvc.Http;
@@ -13,12 +12,13 @@ import java.util.concurrent.CompletionStage;
  * Created by zero on 12/1/17.
  */
 public class ForceHttps extends Action<Controller> {
-    private static boolean isDevMode = Play.isDev(Play.current()) || Play.isTest(Play.current());
+
+    private static boolean enableSSL = (play.Play.application().configuration().getBoolean("enable.ssl"));
 
     @Override
     public CompletionStage<Result> call(Http.Context ctx) {
         final CompletionStage<Result> result;
-        if (!ctx.request().secure() && !isDevMode) {
+        if (!ctx.request().secure() && enableSSL) {
             return CompletableFuture.supplyAsync(() -> redirect("https://" + ctx.request().host() + ctx.request().uri()) );
         }
         else {
