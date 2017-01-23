77e0724e44cb67e85d5c0cffeb8bc200f09861ec
ravi <rvsharma972@gmail.com>
2017-01-23 15:38:50 +0530

    hot fixes

--- public/partner/js/generateJobCard.js
+++ public/partner/js/generateJobCard.js
@@ -1841,7 +1841,7 @@ openPartnerPreScreenModal = function (jobPostId, candidateId) {
 
         // display only Min Requirement
         decorator.textContainers.noteContainer.visibility = false;
-        decorator.textContainers.minReqContainer.className = "col-lg-12 form-group remove-padding-left";
+        decorator.textContainers.minReqContainer.className = "mdl-cell mdl-cell--12-col";
 
         // remove callConnected
         decorator.callYesNoRequired = false;
@@ -1851,7 +1851,7 @@ openPartnerPreScreenModal = function (jobPostId, candidateId) {
             decorator.preScreen.title = "Job Application Form"
         }
         decorator.table.mainTable.title = "Job Requirements : Please verify and update candidate's details ";
-        decorator.table.mainTable.minReqTable.className = "mdl-grid--no-spacing";
+        decorator.table.mainTable.minReqTable.className = "mdl-grid--no-spacing panel-collapse collapse";
         decorator.finalSubmissionButton.className = "mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent modal-submit";
         decorator.table.otherTable.title = "Job Details: ";
         decorator.textContainers.minReqContainer.title = "Other Requirements";
