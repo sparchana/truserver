4a7b22fcc2cc53bd59602f4bf3d1d0c379d00ac1
adarsh <rajshahbvm@yahoo.com>
2017-01-29 15:09:47 +0530

    disabling save button while creating job post in rec and support

--- public/recruiter/js/post_free_job.js
+++ public/recruiter/js/post_free_job.js
@@ -800,6 +800,7 @@ function saveJob() {
                 resumeApplicationDate: jobPostResumeDate
             };
 
+            $("#saveEdit").addClass("disabled");
             $.ajax({
                 type: "POST",
                 url: "/recruiter/api/addJobPost",
@@ -825,6 +826,7 @@ function processDataAddJobPost(returnedData) {
             window.location = "/recruiter/allRecruiterJobPosts";
         }, 2500);
     } else{
+        $("#saveEdit").removeClass("disabled");
         notifyError("Something went wrong. Please try again later!");
     }
 }
